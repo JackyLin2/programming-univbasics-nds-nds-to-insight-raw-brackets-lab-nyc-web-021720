61ce6a57245d775cf103aa653532229c56e7dd52
Jacky Lin <jacky.jia.lin@gmail.com>
2020-02-04 20:26:45 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -10,7 +10,7 @@ while director_index < nds.length do
   totals[director_name] = 0 
   movie_index = 0 
   while movie_index < nds[director_index][:movies].length do 
-     total[director_name] += nds[director_index][:movies][movie_index][:worldwide_gross]
+     totals[director_name] += nds[director_index][:movies][movie_index][:worldwide_gross]
      movie_index += 1 
   end 
   director_index += 1
