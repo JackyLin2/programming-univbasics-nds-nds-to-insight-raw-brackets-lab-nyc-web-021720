740b56f4f2af8e902ce03d2f41f50107c1f8a499
Jacky Lin <jacky.jia.lin@gmail.com>
2020-02-04 20:19:46 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -3,21 +3,20 @@ require 'directors_database'
 
 def directors_totals(nds)
  pp nds
-    director_index = 0 
-    totals ={}
-  while director_index < nds.length do 
+  director_index = 0
+  }	 totals = {}
+ while director_index < nds.length do
    director_name = nds[director_index][:name]
-   totals[director_name] = 0 
-   movie_index = 0 
-  while movie_index < nds[director_index][:movies].length do 
-    totals[director_name] +- nds[director_index][:movies][movie_index][:worldwide_gross]
-    movie_index += 1 
-  end 
-    director_index += 1 
-end
-totals 
-end
-
+   totals[director_name] = 0
+   movie_index = 0
+   while movie_index < nds[director_index][:movies].length do
+     totals[director_name] += nds[director_index][:movies][movie_index][:worldwide_gross]
+     movie_index += 1
+   end
+   director_index += 1
+ end
+ totals
+end 
 
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
